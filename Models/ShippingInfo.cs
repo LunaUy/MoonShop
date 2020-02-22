7277c0898488fe12963d194cc44b14531c4c6107
Bart Calixto <bart@dot.uy>
2020-02-22 19:28:50 -0300

    WIP

---
+++ Models/ShippingInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoonShop.Models
+{
+    public class ShippingDetail
+    {
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "El número de teléfono no parece ser válido")]
+        public string Phone { get; set; }
+
+    }
+}
---
+++ Services/CartService.cs
@@ -0,0 +1,30 @@
+﻿using MoonShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoonShop.Services
+{
+    public class CartService
+    {
+        List<Product> Products { get; set; } = new List<Product>();
+
+
+        public async Task<List<Product>> GetProducts()
+        {
+            await Task.CompletedTask;
+            return Products;
+        }
+
+        public Task AddToCart(Product product)
+        {
+            if (!Products.Contains(product))
+            {
+                Products.Add(product);
+            }
+            return Task.CompletedTask;
+        }
+
+    }
+}
